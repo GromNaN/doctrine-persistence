2a8f3d5d9e0c2f93b8f626f4d23be9e74090d961
Jérôme Tamarelle <jerome@tamarelle.net>
2025-10-10 18:48:32 +0200

    Use PHP_VERSION_ID to enable the compilation optimization

--- src/Persistence/Mapping/RuntimeReflectionService.php
+++ src/Persistence/Mapping/RuntimeReflectionService.php
@@ -14,21 +14,14 @@ use function array_key_exists;
 use function assert;
 use function class_exists;
 use function class_parents;
-use function phpversion;
-use function version_compare;
+
+use const PHP_VERSION_ID;
 
 /**
  * PHP Runtime Reflection Service.
  */
 class RuntimeReflectionService implements ReflectionService
 {
-    private readonly bool $supportsTypedPropertiesWorkaround;
-
-    public function __construct()
-    {
-        $this->supportsTypedPropertiesWorkaround = version_compare(phpversion(), '7.4.0') >= 0;
-    }
-
     /**
      * {@inheritDoc}
      */
@@ -75,7 +68,7 @@ class RuntimeReflectionService implements ReflectionService
     {
         $reflectionProperty = new RuntimeReflectionProperty($class, $property);
 
-        if ($this->supportsTypedPropertiesWorkaround && ! array_key_exists($property, $this->getClass($class)->getDefaultProperties())) {
+        if (PHP_VERSION_ID >= 70400 && ! array_key_exists($property, $this->getClass($class)->getDefaultProperties())) {
             $reflectionProperty = new TypedNoDefaultReflectionProperty($class, $property);
         }
 
